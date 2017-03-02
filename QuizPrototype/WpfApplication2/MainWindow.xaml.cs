5ad80a4da43dada9e82f2ec5a3d33e1217215459
Lancelei <lancelei.h@hotmail.ca>
2017-03-02 10:56:39 -0800

    Answers are now randomized and each Question has their own answers

--- QuizPrototype/WpfApplication2/MainWindow.xaml.cs
+++ QuizPrototype/WpfApplication2/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace WpfApplication2
     public partial class MainWindow : Window
     {
         private List<Question> questions = new List<Question>();
-        private List<string> answers = new List<string>();
+        //private List<string> answers = new List<string>();
         private List<RadioButton> possibleAnswers = new List<RadioButton>();
         private int questNum;
         private static int score;
@@ -36,17 +36,19 @@ namespace WpfApplication2
 
         private void start()
         {
+            Random rnd = new Random();
+            string[] answers = questions[questNum].Answers.OrderBy(x => rnd.Next()).ToArray();
+            
             radioButton1.Content = answers[0];
             radioButton2.Content = answers[1];
             radioButton3.Content = answers[2];
             radioButton4.Content = answers[3];
+
             questionLabel.Content = questions[questNum].Quest;
-            answerLabel.Content = questions[questNum].Answer;
+            answerLabel.Content = questions[questNum].CorrectAnswer;
             answerButton.IsEnabled = true;
             saveButton.IsEnabled = false;
             nextButton.IsEnabled = false;
-            
-
             
         }
 
@@ -58,18 +60,41 @@ namespace WpfApplication2
             possibleAnswers.Add(radioButton3);
             possibleAnswers.Add(radioButton4);
             
-
-            answers.Add("Encapsulation");
-            answers.Add("Properties");
-            answers.Add("CLR");
-            answers.Add("C#");
-
-            Random rand = new Random();
-            questions.Add(new Question("What is another term for data hiding?", answers[0]));
-            questions.Add(new Question("The data stored in an object are commonly called fields or:)", answers[1]));
-            questions.Add(new Question("What compiles ILR code into machine language?", answers[2]));
-
-
+            questions.Add(new Question("What is another term for data hiding?", "Encapsulation"));
+            List<string> answers1 = new List<string>();
+            answers1.Add("Boxing");
+            answers1.Add("Encapsulation");
+            answers1.Add("Fielding");
+            answers1.Add("Protected");
+            questions[0].Answers = answers1;
+            questions.Add(new Question("The data stored in an object are commonly called fields or:", "Properties"));
+            List<string> answers2 = new List<string>();
+            answers2.Add("Properties");
+            answers2.Add("Operators");
+            answers2.Add("Destructors");
+            answers2.Add("Value Type");
+            questions[1].Answers = answers2;
+            questions.Add(new Question("What compiles ILR code into machine language?", "CLR"));
+            List<string> answers3 = new List<string>();
+            answers3.Add("JIT");
+            answers3.Add("JTI");
+            answers3.Add("Destructor");
+            answers3.Add("CLR");
+            questions[2].Answers = answers3;
+            questions.Add(new Question("What are the setters and getters for fields called?", "Accessors"));
+            List<string> answers4 = new List<string>();
+            answers4.Add("Properties");
+            answers4.Add("Values");
+            answers4.Add("Accessors");
+            answers4.Add("Specifiers");
+            questions[3].Answers = answers4;
+            questions.Add(new Question("Which of the following is the default access specifier of a class member variable?", "Private"));
+            List<string> answers5 = new List<string>();
+            answers5.Add("Protected");
+            answers5.Add("Protected Internal");
+            answers5.Add("Private");
+            answers5.Add("Default");
+            questions[4].Answers = answers5;
 
         }
 
@@ -79,7 +104,7 @@ namespace WpfApplication2
             {
                 if(radioBtn.IsChecked == true)
                 {
-                    if(radioBtn.Content.ToString() == questions[questNum].Answer)
+                    if(radioBtn.Content.ToString() == questions[questNum].CorrectAnswer)
                     {
                         radioBtn.Background = Brushes.Green;
                         score++;
@@ -87,15 +112,16 @@ namespace WpfApplication2
                     {
                         radioBtn.Background = Brushes.Red;
                     }
+                    answerButton.IsEnabled = false;
+                    saveButton.IsEnabled = true;
+                    nextButton.IsEnabled = true;
+                    answerLabel.Visibility = Visibility.Visible;
                     break;
 
                 }
                 
             }
-            answerButton.IsEnabled = false;
-            saveButton.IsEnabled = true;
-            nextButton.IsEnabled = true;
-            answerLabel.Visibility = Visibility.Visible;
+            
         }
 
         private void nextButton_Click(object sender, RoutedEventArgs e)
@@ -123,12 +149,13 @@ namespace WpfApplication2
     public class Question
     {
         private string quest;
-        private string answer;
+        private string correctAnswer;
+        private List<string> answers = new List<string>();
 
         public Question(string question, string ans)
         {
             quest = question;
-            answer = ans;
+            correctAnswer = ans;
         }
 
         public string Quest
@@ -143,16 +170,29 @@ namespace WpfApplication2
             }
         }
 
-        public string Answer
+        public string CorrectAnswer
         {
             get
             {
-                return answer;
+                return correctAnswer;
             }
 
             set
             {
-                answer = value;
+                correctAnswer = value;
+            }
+        }
+
+        public List<string> Answers
+        {
+            get
+            {
+                return answers;
+            }
+
+            set
+            {
+                answers = value;
             }
         }
         
